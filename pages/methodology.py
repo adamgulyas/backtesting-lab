6f40381f35a71e9622880f28fe3aed7099658760
unknown <roger.hull@ucalgary.ca>
2022-06-19 13:15:21 -0700

    updating the methodology tab

--- pages/methodology.py
+++ pages/methodology.py
@@ -19,15 +19,20 @@ with nlp:
     st.header('Natural Language Processing')
     st.write('You could study techincal stock trading charts for years and still get blind-sided by news that takes the market for a ride (downward). This is where Natural Language Processing (NLP) comes in. Whenever a New York Times (NYT) article is posted, our algorithm will read the article and determine if it has a positive or negative sentiment which could impact the trend of the stock market. In the case that great or terrible news comes out, our algorithm will tell you how we think the market will move based on that data.')
     st.write('We utilized the NYT API to collect the news articles and an NLP technology called Vader to determine the sentiment of each article. After we determined the sentiment of each article on each day, we used classification to see how it impacts the stock price')
-    
-    df = pd.read_csv('aapl.csv')
-    df
+    st.subheader('NYT Sentiment')
+    st.write('Each date in the below dataframe represents one new article we analyzed for sentiment. A 1 represents a positive sentiment and a 0 represents a negative sentiment')
+    sentiment_df = pd.read_csv('Sentiment_Signals/simple_cvd_signal.csv')
+    sentiment_df
     
 with technical_analysis:
     st.header('Technical Analysis')
-    st.write('Humans are creatures of habit. We like to pretend that we can predict what will happen in the stock market based on what has happened in the past. As false as this is, it ends up being a self-fulfiling prophecy where if enough people follow a similar strategy, patterns in the market will persist that are somewhat reliable. In our algorithm, we rely on statistics to inform us about how the market is moving. We use the Exponentially Weighted Moving Average (EWMA), the Slow Moving Average (SMA), Moving Average Convergaence Divergence (MACD), and Bollinger Bands to determine where the stock is currently headed (up or down) and by how much. If the algorithm sees that the stock is moving upward and is trading close to the mean closing price, that would produce a signal to buy the stock. If the stock is trending up but is more than one standard deviation away from the mean (at the upper end of the Bollinger Band), that would produce a signal to sell.')
+    st.write('Humans are creatures of habit. We like to pretend that we can predict what will happen in the stock market based on what has happened in the past. As false as this is, it ends up being a self-fulfiling prophecy where if enough people follow a similar strategy, patterns in the market will emerge that are somewhat reliable. In our algorithm, we rely on statistics to inform us about how the market is moving. We use the Exponentially Weighted Moving Average (EWMA), the Slow Moving Average (SMA), Moving Average Convergaence Divergence (MACD), and Bollinger Bands to determine where the stock is currently headed (up or down) and by how much. If the algorithm sees that the stock is moving upward and is trading close to the mean closing price, that would produce a signal to buy the stock. If the stock is trending up but is more than one standard deviation away from the mean (at the upper end of the Bollinger Band), that would produce a signal to sell.')
+    
     
 with svm:
     st.header('Putting It All Together')
     st.write('By combining the information that we collect through NLP and technical analysis, we can hopefully determine what direction the stock is trading, whether it is moving erratically, and what the general sentiment of the particular investment vehicle may be.')
-    
+    st.subheader('SVM Model')
+    st.write('See the below columns that our machine learning model was trained on')
+    svm_df = pd.read_csv('svm_signals.csv')
+    svm_df
