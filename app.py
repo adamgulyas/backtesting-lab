6eec78b39d03ebd3d7ae04799e4f0943bf22746e
unknown <roger.hull@ucalgary.ca>
2022-06-19 13:51:19 -0700

    form v6

--- app.py
+++ app.py
@@ -91,22 +91,25 @@ with option_select:
                 min_value=100,
                 max_value=10000000
             )
+        col6 = st.columns(1)
+        
+        with col6:
+            is_sentiment = st.checkbox('NY Times Sentiment Analysis')
 
         submitted = st.form_submit_button('Run My Algorithm')
     
 with option_select_dmac:
-    col6, col7, col8 = st.columns(3)
+    col7, col8, = st.columns(2)
 
-    with col6:
+    with col7:
         # st.write("Fast SMA Window")
         fast_window = st.slider('Fast SMA Window',0,60,4) #min: 0, max:60, def:4
 
-    with col7:
+    with col8:
         # st.write('Slow SMA Window')
         slow_window = st.slider('Slow SMA Window',0,180,100) #min: 0, max:180, def:100
 
-    with col8:
-        is_sentiment = st.checkbox('NY Times Sentiment Analysis')
+        
 
 
 #with run:
